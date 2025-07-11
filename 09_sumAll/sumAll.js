ebaff668167f80fdede582b28083d275dcd9a8fe
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-11 20:54:23 +0530

    COMPLETED 09 SUMALL EXERCISE

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const checkA = !Number.isInteger(a) || a < 0;
+    const checkB = !Number.isInteger(b) || b < 0;
+    let total = 0;
 
+    if (checkA || checkB) {
+        return "ERROR"
+    }
+
+    (a > b) ? [a, b] = [b, a] : "";
+
+    for (let i=a; i<b+1; i++){
+        total += i;
+    }
+
+    return total;
+    // return (b-a+1) * (a+b) / 2;
 };
 
 // Do not edit below this line
