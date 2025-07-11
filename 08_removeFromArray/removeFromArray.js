2b65da923cb8f2427affae944175487b5478777a
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-11 20:30:38 +0530

    COMPLETE 08 REMOVEFROMARRAY EXERCISE

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr;
+    for (item of args) {
+        newArr = (newArr || arr).filter(ele => ele !== item);
+    }
+    return newArr;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
