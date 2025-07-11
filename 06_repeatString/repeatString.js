68dd4b705c600cf8a3e9605d38de378e4fa207bd
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-11 19:42:53 +0530

    COMPLETE 06 REPEATSTRING EXERCISE

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    if (times < 0){
+        return "ERROR"
+    };
+    
+    let repeatText = "";
+    for (let i=0; i<times; i++) {
+        repeatText += text;
+    }
+    return repeatText;
 };
-
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
