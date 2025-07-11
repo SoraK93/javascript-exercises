4ed3a134094a215d222ebd7941880af6061b6102
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-11 21:16:10 +0530

    COMPLETE 10 LEAPYEARS EXCERSICE

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // year / 4 & 400
+  // ! year / 100
+  const divisibleBy4 = year % 4 === 0;
+  const divisibleBy400 = year % 400 === 0;
+  const notDivisibleBy100 = year % 100 !== 0;
+  return divisibleBy400 || (divisibleBy4 && notDivisibleBy100);
 };
 
 // Do not edit below this line
