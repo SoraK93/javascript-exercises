b73c40bb46ad0a24dd34599199849473ae7e3ab9
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-11 19:51:23 +0530

    COMPLETE 07 REVERSESTRING EXERCISE

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let textReverse = "";
 
+    if (!text) {
+        return textReverse;
+    }
+    
+    for(let i = text.length - 1; i >= 0; i--){
+        textReverse += text[i];
+    }
+
+    return textReverse;
 };
 
 // Do not edit below this line
